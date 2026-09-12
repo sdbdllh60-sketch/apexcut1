0a2e1fbbd5adbebdfb353cc7cfdca558f841e642
عبدالله <sdbdllh60@gmail.com>
2026-09-12 11:49:19 +0300

    Update build.gradle.kts

--- apexcut (2)/app/build.gradle.kts
+++ apexcut (2)/app/build.gradle.kts
@@ -31,12 +31,16 @@ android {
       keyAlias = "upload"
       keyPassword = System.getenv("KEY_PASSWORD")
     }
-    create("debugConfig") {
-      storeFile = file("${rootDir}/debug.keystore")
-      storePassword = "android"
-      keyAlias = "androiddebugkey"
-      keyPassword = "android"
     }
+signingConfigs {
+    create("release") {
+        val keystorePath = System.getenv("KEYSTORE_PATH") ?: "${rootDir}/my-upload-key.jks"
+        storeFile = file(keystorePath)
+        storePassword = System.getenv("STORE_PASSWORD")
+        keyAlias = "upload"
+        keyPassword = System.getenv("KEY_PASSWORD")
+    }
+}
   }
 
   buildTypes {
